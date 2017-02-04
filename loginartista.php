b8fa8e5f6bac99974c0eba9420d46032743c1a27
Benji <santsben@gmail.com>
2017-02-04 04:58:57 -0500

    rrewr

--- loginartista.php
+++ loginartista.php
@@ -1,28 +1,32 @@
 <!DOCTYPE html>
-<html>
-<head>
-<link rel="stylesheet" type="text/css" href="styleartista.css">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title></title>
+<html class="gr__leftherian_tk"><head>
+<title>Inicio Leftherean</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+
+<link href="styleartista.css" rel="stylesheet" type="text/css" media="all">
+
 </head>
-<body>
-	<div class="divicono">
-		<div class="icono">
-		<img src="img/iconoartista.png">
-		</div>
-	</div>
-	<div class="divform">
-<div class="container">
-	<form>
-	 	<label>Usuario</label>
-		<input type="text" class="input" name="nombre" value=" "><br>
-		<hr />
-		<label>Contraseña</label>
-		<input type="password" class="input" name="nombre" >
-		<hr />
-		<input type="submit" name="submit" id="submit" value="Ingresar">
-	</form>
-</div>
-</div>
-</body>
-</html>
+<body data-gr-c-s-loaded="true">
+
+	<div class="w3agileits-main">
+		<div class="agileits-newsletter">
+			<div class="w3ls-tabs"> 
+				<img style="padding-bottom: 80px" src="img/iconoartista.png" alt="">
+	
+				<div class="">
+					<form>
+						<span>Usuario</span>
+						<input type="text" name="user"><hr><br>
+						<span>Contraseña</span>
+						<input type="text" name="password"><hr>
+						<input type="submit" name="submit" id="submitartista" value="Ingresar">
+					</form>
+				</div>
+			</div>	
+		</div>	
+	</div>	
+	
+
+</body></html>
