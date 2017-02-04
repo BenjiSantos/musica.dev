39944a6a6565136bcdd4bf3006f771f4d50a347c
Benji <santsben@gmail.com>
2017-02-04 03:47:43 -0500

    Login artista

---
+++ loginartista.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="styleartista.css">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title></title>
+</head>
+<body>
+	<div class="divicono">
+		<div class="icono">
+		<img src="img/iconoartista.png">
+		</div>
+	</div>
+	<div class="divform">
+<div class="container">
+	<form>
+	 	<label>Usuario</label>
+		<input type="text" class="input" name="nombre" value=" "><br>
+		<hr />
+		<label>Contraseña</label>
+		<input type="password" class="input" name="nombre" >
+		<hr />
+		<input type="submit" name="submit" id="submit" value="Ingresar">
+	</form>
+</div>
+</div>
+</body>
+</html>
